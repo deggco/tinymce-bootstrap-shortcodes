4f073c2c17cbb3a6249ea41958e05ec9e2aa83b4
Travis Scheidegger <t@degg.co>
2014-06-23 10:29:08 -0700

    Refactor code

---
+++ tinymce-bootstrap-shortcodes.js
@@ -0,0 +1,186 @@
+(function() {
+  tinymce.PluginManager.add('TMCEBS_shortcodes', function(editor, url) {
+    // Add a button that opens a window and adds a URL
+    editor.addButton('TMCEBS_URL_shortcode_key', {
+      //text: 'Button',
+      icon: 'dashicons icon-link',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Bootstrap Button Shortcode',
+          body: [{
+            type: 'textbox',
+            name: 'title',
+            label: 'Title'
+          }, {
+            type: 'textbox',
+            name: 'url',
+            label: 'URL'
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[button title=\'' + e.data.title + '\' url=\'' + e.data.url + '\']');
+            //[button title='title' url='url']
+          }
+        });
+      }
+    });
+    // Add a button that opens a window
+    editor.addButton('TMCEBS_col_6_shortcode_key', {
+      //text: 'Button',
+      icon: 'dashicons icon-layout3',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Shortcode 1:1',
+          body: [{
+            type: 'textbox',
+            name: 'col1',
+            label: 'Column 1',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }, {
+            type: 'textbox',
+            name: 'col2',
+            label: 'Column 2',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[row][col-6]' + e.data.col1 + '[/col-6][col-6]' + e.data.col2 + '[/col-6][/row]');
+          }
+        });
+      }
+    });
+    //col-8/col-4
+    // Add a button that opens a window
+    editor.addButton('TMCEBS_col_84_shortcode_key', {
+      //text: 'Button',
+      icon: 'dashicons icon-layout2',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Shortcode 2:1',
+          body: [{
+            type: 'textbox',
+            name: 'col1',
+            label: 'Column 1',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }, {
+            type: 'textbox',
+            name: 'col2',
+            label: 'Column 2',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[row][col-8]' + e.data.col1 + '[/col-8][col-4]' + e.data.col2 + '[/col-4][/row]');
+          }
+        });
+      }
+    });
+    //col-4/col-8
+    // Add a button that opens a window
+    editor.addButton('TMCEBS_col_48_shortcode_key', {
+      //text: 'Button',
+      icon: 'dashicons icon-layout',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Shortcode 1:2',
+          body: [{
+            type: 'textbox',
+            name: 'col1',
+            label: 'Column 1',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }, {
+            type: 'textbox',
+            name: 'col2',
+            label: 'Column 2',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[row][col-4]' + e.data.col1 + '[/col-4][col-8]' + e.data.col2 + '[/col-8][/row]');
+          }
+        });
+      }
+    });
+    //col-4/col-4/col-4
+    // Add a button that opens a window
+    editor.addButton('TMCEBS_col_444_shortcode_key', {
+      //text: 'Button',
+      icon: 'dashicons icon-layout4',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Shortcode 1:1:1',
+          body: [{
+            type: 'textbox',
+            name: 'col1',
+            label: 'Column 1',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }, {
+            type: 'textbox',
+            name: 'col2',
+            label: 'Column 2',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }, {
+            type: 'textbox',
+            name: 'col3',
+            label: 'Column 3',
+            multiline: true,
+            minWidth: 300,
+            minHeight: 100
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[row][col-4]' + e.data.col1 + '[/col-4][col-4]' + e.data.col2 + '[/col-4][col-4]' + e.data.col3 + '[/col-4][/row]');
+          }
+        });
+      }
+    });
+    //col-4/col-4/col-4
+    // Add a button that opens a window
+    editor.addButton('TMCEBS_clearer', {
+      //text: 'Button',
+      icon: 'dashicons icon-layout4',
+      //icon: false,
+      onclick: function() {
+        // Open window
+        editor.windowManager.open({
+          title: 'Clear floats',
+          body: [{
+            type: 'textbox',
+            name: 'vertical-lines',
+            label: 'Vertical lines',
+          }],
+          onsubmit: function(e) {
+            // Insert content when the window form is submitted
+            editor.insertContent('[clear vertical-lines=' + e.data.vertical-lines + ']');
+          }
+        });
+      }
+    });
+  });
+})();
---
+++ tinymce-bootstrap-shortcodes.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Plugin Name: TinyMCE Bootstrap Shortcodes
+ * Description: A plugin to add a custom button and shortcode to TinyMCE
+ * Plugin URI:  https://github.com/zero4281/tinymce-bootsrap-shortcodes
+ * Version:     0.0.1
+ * Author:      Joshua Rising
+ * Author URI:  https://github.com/zero4281/
+ * License:     GPL3
+ * Copy Right: 2014 Joshua Rising, Rogue Valley Software
+ */
+
+function TMCEBS_css() {
+  wp_enqueue_style('TMCEBS_css', plugins_url('/style.css', __FILE__));
+}
+
+add_action('admin_enqueue_scripts', 'TMCEBS_css');
+
+add_action( 'admin_head', 'TMCEBS_add_tinymce' );
+function TMCEBS_add_tinymce() {
+  global $typenow;
+
+  // only on Post Type: post and page
+  if( ! in_array( $typenow, apply_filters('TMCEBS_post_types', array( 'post', 'page' ) ) ) )
+    return ;
+
+  add_filter( 'mce_external_plugins', 'TMCEBS_add_tinymce_plugin' );
+  // Add to line 1 form WP TinyMCE
+  add_filter( 'mce_buttons', 'TMCEBS_add_tinymce_button' );
+}
+
+// inlcude the js for tinymce
+function TMCEBS_add_tinymce_plugin( $plugin_array ) {
+  $plugin_array['TMCEBS_button'] = plugins_url( '/tinymce-bootstrap-shortcodes.js', __FILE__ );
+  // Print all plugin js path
+  //var_dump( $plugin_array );
+  return $plugin_array;
+}
+
+// Add the button key for address via JS
+function TMCEBS_add_tinymce_button( $buttons ) {
+
+  array_push( $buttons, 'TMCEBS_URL_shortcode_key' );
+  array_push( $buttons, 'TMCEBS_col_6_shortcode_key' );
+  array_push( $buttons, 'TMCEBS_col_48_shortcode_key' );
+  array_push( $buttons, 'TMCEBS_col_84_shortcode_key' );
+  array_push( $buttons, 'TMCEBS_col_444_shortcode_key' );
+  // Print all buttons
+  //var_dump( $buttons );
+  return $buttons;
+}
+/*
+TMCEBS_URL_shortcode_key
+*/
+//function to output shortcode
+function TMCEBS_display_link($atts) {
+  @extract($atts);
+  $title;
+  $url;
+  if(stripos($url, 'http') === FALSE) {
+    $url = 'http://' . $url;
+  }
+  $tempLink = "<a class=\"btn btn-primary\" title=\"$title\" href=\"$url\" target='_blank'>$title</a>";
+
+  return $tempLink;
+}
+//function to output shortcode
+function TMCEBS_shortcode_display_row($atts, $content = null) {
+  $tempStr = '<div class="row">'.do_shortcode($content).'</div>';
+  return $tempStr;
+}
+function TMCEBS_shortcode_display_col_6($atts, $content = null) {
+  $tempStr = '<div class="col-md-6">'.do_shortcode($content).'</div>';
+  return $tempStr;
+}
+function TMCEBS_shortcode_display_col_8($atts, $content = null) {
+  $tempStr = '<div class="col-md-8">'.do_shortcode($content).'</div>';
+  return $tempStr;
+}
+function TMCEBS_shortcode_display_col_4($atts, $content = null) {
+  $tempStr = '<div class="col-md-4">'.do_shortcode($content).'</div>';
+  return $tempStr;
+}
+
+add_shortcode('button', 'TMCEBS_shortcode_display_link');
+add_shortcode('row', 'TMCEBS_shortcode_display_row');
+add_shortcode('col-6', 'TMCEBS_shortcode_display_col_6');
+add_shortcode('col-8', 'TMCEBS_shortcode_display_col_8');
+add_shortcode('col-4', 'TMCEBS_shortcode_display_col_4');
+?>
