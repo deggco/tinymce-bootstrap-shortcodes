9841e4466a4052d03184d6bf49a5d3c72ab551b3
Daniel Royston <d@nielnr.com>
2014-07-10 17:15:35 -0700

    Added responsive sliders // Shortcode generation

--- js/insert.js
+++ js/insert.js
@@ -94,9 +94,98 @@ function calcValues(values,columnWidthOptions) {
 }
 
 function processForm(obj) {
+
+  var responsive = jQuery("#responsive-design")[0].checked;
+  
+  var data = {
+    text: {
+      "1": jQuery("textarea[name='col1-text']").val(),
+      "2": jQuery("textarea[name='col2-text']").val()
+    },
+    responsive: responsive,
+  }
+
+  if(columnCount > 2)
+    data["text"]["3"] = jQuery("textarea[name='col3-text']").val();
+  if(columnCount > 3)
+    data["text"]["4"] = jQuery("textarea[name='col4-text']").val();
+
+  if(responsive) {
+    var size = {
+      xs: {
+        '1': jQuery("input[name='xs-col1-width']").val(),
+        '2': jQuery("input[name='xs-col2-width']").val()
+      },
+      sm: {
+        '1': jQuery("input[name='sm-col1-width']").val(),
+        '2': jQuery("input[name='sm-col2-width']").val()
+      },
+      md: {
+        '1': jQuery("input[name='md-col1-width']").val(),
+        '2': jQuery("input[name='md-col2-width']").val()
+      },
+      lg: {
+        '1': jQuery("input[name='lg-col1-width']").val(),
+        '2': jQuery("input[name='lg-col2-width']").val()
+      }
+    }
+    if(columnCount > 2) {
+      size['xs']['3'] = jQuery("input[name='xs-col3-width']").val();
+      size['sm']['3'] = jQuery("input[name='sm-col3-width']").val();
+      size['md']['3'] = jQuery("input[name='md-col3-width']").val();
+      size['lg']['3'] = jQuery("input[name='lg-col3-width']").val();
+    }if(columnCount > 3) {
+      size['xs']['4'] = jQuery("input[name='xs-col4-width']").val();
+      size['sm']['4'] = jQuery("input[name='sm-col4-width']").val();
+      size['md']['4'] = jQuery("input[name='md-col4-width']").val();
+      size['lg']['4'] = jQuery("input[name='lg-col4-width']").val();
+    }
+    data.size = size;
+  }else{
+    var size = {
+      '1': jQuery("input[name='col1-width']").val(),
+      '2': jQuery("input[name='col2-width']").val()
+    }
+
+    if(columnCount > 2)
+      size['3'] = jQuery("input[name='col3-width']").val();
+    if(columnCount > 3)
+      size['4'] = jQuery("input[name='col4-width']").val();
+
+    data["size"] = size;
+  }
+
+  console.log(generateShortcode(data));
+
+  return false;
   
 }
 
-function generateShortcode() {
+function generateShortcode(data) {
+
+  shortcode = "[row]";
+  sizes = ['xs','sm','md','lg'];
+
+  for(i=0;i<columnCount;i++) {
+    classes = ""
+    if(data['responsive']) {
+      for(j=0;j<4;j++) {
+        size = data["size"][sizes[j]][i+1];
+        if(size == 0)
+          classes += "hidden-"+sizes[j] + " ";
+        else
+          classes += "col-"+sizes[j]+"-"+size;
+        if(4 - j !== 1)
+          classes += " ";
+      }
+    }else{
+      classes += "col-xs-"+data["size"][i+1];
+    }
+    shortcode += "[col class=\""+classes+"\"]"+data["text"][i+1]+"[/col]";
+  }
+
+  shortcode += "[/row]";
+
+  return shortcode;
 
 }
